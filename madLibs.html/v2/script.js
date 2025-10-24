b8ba6f977bff84fdbc596cd0b62a760ecf9aa507
empelaez-star <empelaez@ucdavis.edu>
2025-10-23 20:29:26 -0700

    j3

--- madLibs.html/v2/script.js
+++ madLibs.html/v2/script.js
@@ -1,10 +1,59 @@
+// (function(){
+//     'use strict';
+//     console.log('reading js');
+
+//     const myform = document.querySelector('#myform');
+//     const madlb = document.querySelector('#madlib');
+//     const overlay = document.querySelector('#overlay');
+//     const overlayArticle = document.querySelector('article');
+   
+
+
+//     myform.addEventListener('submit', function(event){
+
+//         event.preventDefault();
+    
+//        //document.querySelector('#overlay').className = 'showing';
+//         overlay.classList.remove('hidden');
+//         overlay.classList.add('showing');
+//         //myform.classList.add('hidden');
+//         //myform.classList.remove('showing');
+        
+
+//         const yourName = document.querySelector('#yourName').value;
+//         const abnoun = document.querySelector('#AbstractNoun').value;
+//         const pnoun = document.querySelector('#pluralNoun').value;
+//         const adj = document.querySelector('#adjective').value;
+//         const ptverb = document.querySelector('#ptVerb').value;
+//         const noun1 = document.querySelector('#noun').value;
+
+//         const myText = `here are your words: ${yourName} ${abnoun} ${pnoun} ${adj} ${ptverb} ${noun1}`;
+//         console.log(myText);
+    
+
+        
+
+//     overlay.innerHTML = `Your Confession! ${myText}`;
+    
+
+
+
+        
+      
+
+
+//     });
+// })
+
+
+
 (function(){
     'use strict';
     console.log('reading js');
 
-    const myform = document.querySelector('#myform');
+        const myform = document.querySelector('#myform');
     const madlb = document.querySelector('#madlib');
-    const myoverlay = document.querySelector('#overlay');
+    const overlay = document.querySelector('#overlay');
     const overlayArticle = document.querySelector('article');
    
 
@@ -13,9 +62,9 @@
 
         event.preventDefault();
     
-       document.querySelector('#overlay').className = 'showing';
-        //myoverlay.classList.remove('hidden');
-        //myoverlay.classList.add('showing');
+       //document.querySelector('#overlay').className = 'showing';
+        overlay.classList.remove('hidden');
+        overlay.classList.add('showing');
         //myform.classList.add('hidden');
         //myform.classList.remove('showing');
         
@@ -27,20 +76,20 @@
         const ptverb = document.querySelector('#ptVerb').value;
         const noun1 = document.querySelector('#noun').value;
 
-        const myText = `here are your words: ${yourName} ${abnoun} ${pnoun} ${adj} ${ptverb} ${noun1}`;
+        const myText = `My name’s ${yourName}, and I came here looking for ${abnoun}, not ${pnoun}. But somehow, that’s all I’ve found. My last date was so ${adj}, I almost ${ptverb} right then and there. If I don’t get a rose tonight, I swear I’m leaving to find someone who actually appreciates ${noun1}!`;
+        console.log(myText);
     
 
         
 
-    myoverlay.innerHTML = `Your Confession! ${myText}`;
+    overlay.innerHTML = `YOUR CONFESSION! <br><br> ${myText}`;
     
 
 
 
         
       
-
-
-    });
-})
+    
+  });
+})()
 
